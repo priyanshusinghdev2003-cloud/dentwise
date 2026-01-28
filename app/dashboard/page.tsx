4abee1ba3d11c5827d3aa2d18fbb3c18d26f26ff
Priyanshu Singh <priyanshusinghdev2003@gmail.com>
2026-01-28 21:18:34 +0530

    added dashboard

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,18 @@
+import MainActions from "@/components/dashboard/MainActions";
+import WelcomeSection from "@/components/dashboard/WelcomeSection";
+import Navbar from "@/components/Navbar";
+
+const AboutPage = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="max-w-7xl mx-auto px-6 py-8 pt-24">
+        <WelcomeSection />
+        <MainActions />
+        {/* <ActivityOverview /> */}
+      </div>
+    </>
+  );
+};
+
+export default AboutPage;
